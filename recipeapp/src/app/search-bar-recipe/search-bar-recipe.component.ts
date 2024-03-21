49674e9167d8f9d6c6b85f5870a53bec75fe8c49
SimretN <134206815+SimretN@users.noreply.github.com>
2024-03-21 15:14:49 +0100

    first commit for angular application

---
+++ recipeapp/src/app/search-bar-recipe/search-bar-recipe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar-recipe',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-bar-recipe.component.html',
+  styleUrl: './search-bar-recipe.component.css'
+})
+export class SearchBarRecipeComponent {
+
+}
