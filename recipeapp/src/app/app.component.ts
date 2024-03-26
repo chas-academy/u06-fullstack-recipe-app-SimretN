cd44474dfb8f077bd543e2dc9bfd3bc9c4df1632
SimretN <134206815+SimretN@users.noreply.github.com>
2024-03-26 13:08:48 +0100

    created search component

--- recipeapp/src/app/app.component.ts
+++ recipeapp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, } from '@angular/router';
+import { SearchBarRecipeComponent } from './search-bar-recipe/search-bar-recipe.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBarRecipeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
