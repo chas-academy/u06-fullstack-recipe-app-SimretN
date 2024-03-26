0d6c1565ff4e75dc5563c416932d195d0e1b6ac7
SimretN <134206815+SimretN@users.noreply.github.com>
2024-03-26 22:57:38 +0100

    Basic routing

--- recipeapp/src/app/app.routes.ts
+++ recipeapp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'', component:RecipeComponent},
+    { path:'recipe:id', component:RecipeComponent},
+    { path:'profile', component:ProfileComponent}
+];
