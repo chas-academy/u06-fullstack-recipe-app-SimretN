71d60249539f58e3583cf6b522798a620021459e
SimretN <134206815+SimretN@users.noreply.github.com>
2024-04-02 14:59:17 +0200

    changes in signup components

--- recipeapp/src/app/app.routes.ts
+++ recipeapp/src/app/app.routes.ts
@@ -1,17 +1,15 @@
 import { Routes } from '@angular/router';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { signupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RecipesComponent } from './pages/recipes/recipes.component';
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { authGuard } from './guards/auth.guard';
 
-
 //import { FormsModule } from '@angular/forms';
-  
 
 export const routes: Routes = [
   { path: '', component: RecipesComponent },
-  { path:'recipe/:id', component:RecipeComponent},
-  { path: 'profile', component: ProfileComponent, canActivate: [authGuard]},
+  { path: 'recipe/:id', component: RecipeComponent },
+  { path: 'profile', component: signupComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
 ];
--- recipeapp/src/app/interfaces/user.ts
+++ recipeapp/src/app/interfaces/user.ts
@@ -1,4 +1,5 @@
 export interface User {
+    //registerUser(value: Partial<{ name: string | null; email: string | null; password: string | null; password_confirmation: string | null; }>): unknown;
     id: number,
     name: string,
     email: string,
